6281f85644345e5e14ffd33f744ef8d2f66b85ff
Prince Raj Singh <silence11444@gmail.com>
2025-06-04 08:46:52 +0530

    Update x.py

--- x.py
+++ x.py
@@ -1,6 +1,7 @@
+# Program to create multiple files in series
 import os
 def create(n):
-    for i in range (96,96+n+1):
+    for i in range (1,n+1):
         fname=f"program_{i}.c"
         with open(fname,"w") as f:
             f.write("// hello")
